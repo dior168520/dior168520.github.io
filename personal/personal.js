f8d22d6b2006b5732947edc4acbd3dc4d8085aa4
dior168520 <dior168520@yahoo.com.tw>
2020-06-17 01:06:41 +0800

    personal_2020_06_17

--- personal/personal.js
+++ personal/personal.js
@@ -1,51 +1,43 @@
-  //  collapse漢堡條按鈕
-  var collapse = document.querySelector(".collapse");
-  //  mobile nav 手機漢堡條選單
-   var mobile_nav = document.querySelector(".mobile_nav");
+//  collapse漢堡條按鈕
+var collapse = document.querySelector(".collapse");
+//  mobile nav 手機漢堡條選單
+var mobile_navs = document.querySelector(".mobile_nav");
 
-// close_nav手機選單關閉
-   var  close_nav = document.querySelector(".close_nav");
+// close_nav關閉手機選單按鈕
+var close_nav = document.querySelector(".close_nav");
+//close_list 選單內容
+var close_lists = document.querySelectorAll(".close_list")
 
-    //close_list 選單內容關閉
-    var close_list_A = document.querySelector(".close_list_A")
-    //close_list 選單內容關閉
-    var close_list_B = document.querySelector(".close_list_B")
-    //close_list 選單內容關閉
-    var close_list_C = document.querySelector(".close_list_C")
-    //close_list 選單內容關閉
-    var close_list_D = document.querySelector(".close_list_D")
-    
- 
-// collapse漢堡條按鈕>>點擊>>執行>>打開mobile_navbar手機選單
+
+// collapse漢堡條按鈕=>點擊=>執行=>打開mobile_navbar手機選單
 // addEventListener事件監聽
-collapse.addEventListener("click" , function(){
+collapse.addEventListener("click", function () {
 
-  mobile_nav.classList.add("active");
-});
-
-// close_nav取消按鈕>>點擊>>執行>>關閉mobile_navbar手機選單
-close_nav.addEventListener("click" ,function(){
-
-mobile_nav.classList.remove("active");
+  mobile_navs.classList.add("active");
 });
 
+// close_nav關閉按鈕>>點擊=>執行>>關閉mobile_navbars手機選單
+close_nav.addEventListener("click", function () {
 
-// close_list_A >>點擊>>執行>>關閉mobile_navbar手機選單
-close_list_A.addEventListener("click" , function(){
-  mobile_nav.classList.remove("active");
+  mobile_navs.classList.remove("active");
 });
-// close_list_B >>點擊>>執行>>關閉mobile_navbar手機選單
-close_list_B.addEventListener("click" , function(){
-  mobile_nav.classList.remove("active");
-});
-// close_list_C >>點擊>>執行>>關閉mobile_navbar手機選單
-close_list_C.addEventListener("click" , function(){
-  mobile_nav.classList.remove("active");
-});
-// close_list_D >>點擊>>執行>>關閉mobile_navbar手機選單
-close_list_D.addEventListener("click" , function(){
-  mobile_nav.classList.remove("active");
-});
+
+
+
+
+// mobile 清單
+for (let index = 0; index < close_lists.length; index++) {
+  const close_list = close_lists[index];
+  // console.log(close_list)
+
+//close_list =>點擊=>執行=>關閉清單
+  close_list.addEventListener("click", function () {
+
+    mobile_navs.classList.remove('active');
+
+
+  });
+}
 
 
 
