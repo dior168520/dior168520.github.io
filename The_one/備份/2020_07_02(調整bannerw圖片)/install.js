f74656d5d03e5bf1d9cabdcb1b6dbdc178222240
dior168520 <dior168520@yahoo.com.tw>
2020-07-02 10:40:33 +0800

    2020_07_02_(banner)

---
+++ The_one/備份/2020_07_02(調整bannerw圖片)/install.js
@@ -0,0 +1,28 @@
+// CODELAB: Add event listener for beforeinstallprompt event
+window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
+
+// CODELAB: Add code to save event & show the install button.
+deferredInstallPrompt = evt;
+installButton.removeAttribute('hidden');
+
+// CODELAB: Add code show install prompt & hide the install button.
+deferredInstallPrompt.prompt();
+// Hide the install button, it can't be called twice.
+evt.srcElement.setAttribute('hidden', true);
+
+// CODELAB: Log user response to prompt.
+deferredInstallPrompt.userChoice
+    .then((choice) => {
+        if (choice.outcome === 'accepted') {
+            console.log('User accepted the A2HS prompt', choice);
+        } else {
+            console.log('User dismissed the A2HS prompt', choice);
+        }
+        deferredInstallPrompt = null;
+    });
+
+// CODELAB: Add event listener for appinstalled event
+window.addEventListener('appinstalled', logAppInstalled);
+
+// CODELAB: Add code to log the event
+console.log('the one App was installed.', evt);
