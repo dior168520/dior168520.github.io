a23ceafd4403b1cca4d4bcf7eda9dac6604289dc
dior168520 <64568010+dior168520@users.noreply.github.com>
2020-07-14 16:49:33 +0800

    2020_07_14

---
+++ tea_project/js/tea.js
@@ -0,0 +1,17 @@
+AOS.init(
+
+    );
+
+
+    // navbar animation
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $("nav");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+});
+
+//collapse
+$('nav>div>ul>li>a').on('click', function () {
+    $('.navbar-collapse').collapse('hide');
+});
